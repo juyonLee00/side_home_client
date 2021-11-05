2ba0bd5253c49ee42145c47f7e7ac7ec6c3d0428
변우진 <sforot9@gmail.com>
2021-11-05 14:36:26 +0900

    feat : 세부 수정사항 변경

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import login from './pages/login';
 import { Route, Link } from 'react-router-dom';
 import today_scent from './pages/today_scent';
 import archive from './pages/archive';
-
+import { PickPerfume } from './states/archive';
+import Navi from './components/common/Navi';
 function App() {
   return (
     <div>
-      {/*ul 부분은 지우셔도 상관없습니다. */}
-      <ul>
-        <li>
-          <Link to="login">로그인</Link>
-        </li>
-        <li>
-          <Link to="today">오늘의 향수</Link>
-        </li>
-        <li>
-          <Link to="today">아카이브</Link>
-        </li>
-      </ul>
       <Route path="/login" component={login} />
       <Route path="/today" component={today_scent} />
       <Route path="/main" component={archive} />
