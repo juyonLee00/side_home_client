c304d3b61464394dc01d9b29f322be4935953860
변우진 <sforot9@gmail.com>
2021-11-11 16:45:51 +0900

    feat : 향수상세페이지

--- src/components/archive/archiveDetail.js
+++ src/components/archive/archiveDetail.js
@@ -2,196 +2,199 @@ import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 // recoil
 import { useRecoilState } from 'recoil';
-import { ShowDetailPerfuem } from '../../../src/states/archive';
+import { ShowDetailPerfuem, PickPerfume } from '../../../src/states/archive';
 // icon
-import { AiFillHeart } from 'react-icons/ai';
-
+import { AiFillStar } from 'react-icons/ai';
 const ArchiveDetail = () => {
   const [showItem, setShowItem] = useRecoilState(ShowDetailPerfuem);
+  const [pickItem, setPickItem] = useRecoilState(PickPerfume);
 
   useEffect(() => {
     console.log(showItem);
   });
+  const handleOnPick = (id) => {
+    if (pickItem.includes(id)) {
+      console.log(id);
+      setPickItem(pickItem.filter((item) => item !== id));
+    } else setPickItem(pickItem.concat(id));
+  };
 
   return (
-    <div
-      style={{
-        width: '100%',
-        justifyContent: 'center',
-        textAlign: 'center',
-        verticalAlign: 'center',
-        alignContent: 'center',
-        alignItems: 'center',
-        marginTop: '20rem',
-      }}
-    >
-      <PerfumeCntr>
-        <CodyList>
+    <>
+      <Container>
+        <PerfumeCntr>
           <>
-            <GoodsCntnr>
-              <div
-                style={{
-                  width: '10rem',
-                  height: '11rem',
-                  textAlign: 'center',
-                  alignItems: 'center',
-                  justifyContent: 'center',
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Image
+                className="goods_img"
+                src={showItem.url}
+                // priority={true}
+                // loading="eager"
+                loading="lazy"
+                onContextMenu={() => {
+                  return false;
                 }}
-              >
-                <Image
-                  className="goods_img"
-                  src={showItem.url}
-                  // priority={true}
-                  // loading="eager"
-                  loading="lazy"
-                  width={130}
-                  backgroundColor="red"
-                  height={130}
-                  onContextMenu={() => {
-                    return false;
-                  }}
-                  onDragStart={() => {
-                    return false;
-                  }}
-                />
-              </div>
-              <div
-                style={{
-                  width: '10rem',
-                  height: '4.5rem',
-                  textAlign: 'center',
-                  alignItems: 'center',
-                  justifyContent: 'center',
+                onDragStart={() => {
+                  return false;
                 }}
-              >
-                <Text style={{ marginBottom: '0.2rem' }}>{showItem.name}</Text>
-                <Text isGray={true} style={{ fontWeight: '400' }}>
+              />
+              <BascInfoCntr>
+                <Text isBig>{showItem.name}</Text>
+                <Text isMiddle isGray={true} style={{ fontWeight: '400' }}>
                   {showItem.brand}
                 </Text>
                 <Text isGray={true}>{showItem.type}</Text>
-              </div>
-            </GoodsCntnr>
+              </BascInfoCntr>
+            </div>
+
+            <InfoCntr>
+              <DetailInfo isBold>향조 : {showItem.type}그룹</DetailInfo>
+              <NoteInfo>탑 : {showItem.top.join(',')}</NoteInfo>
+              <NoteInfo>미들 : {showItem.middle.join(',')}</NoteInfo>
+              <NoteInfo isLast>베이스 : {showItem.base.join(',')}</NoteInfo>
+            </InfoCntr>
+
+            <InfoCntr>
+              <DetailInfo isBold>상세설명</DetailInfo>
+              <DetailInfo isGray> {showItem.detail}</DetailInfo>
+            </InfoCntr>
           </>
-        </CodyList>
-      </PerfumeCntr>
-    </div>
+        </PerfumeCntr>
+      </Container>
+      <DockBarCntr>
+        <BtnCntnr>
+          <StarIcon
+            size={'1.5rem'}
+            onClick={() => handleOnPick(showItem.id)}
+            isPick={pickItem && pickItem.includes(showItem.id)}
+          />
+        </BtnCntnr>
+        <BtnCntnr>
+          <BtnText>좋아요(스크랩)</BtnText>
+        </BtnCntnr>
+      </DockBarCntr>
+    </>
   );
 };
 
 export default ArchiveDetail;
 
-const PerfumeCntr = styled.div``;
-const Image = styled.img`
-  background-color: gray;
-`;
-
-const Picture = styled.div`
-  width: 14rem;
-  height: 14rem;
-  margin-right: 1.2rem;
-  border: solid 0.1rem ${({ theme }) => theme.colors.gray_1};
-  border-radius: 0.8rem;
-  background: url(${(props) => props.src}) center center / cover;
-`;
-
-const DelIcon = styled.img`
-  position: relative;
-  top: 0rem;
-  left: 78%;
-
-  width: 3rem;
-  height: 3rem;
-`;
-const CtgCntnr = styled.div`
+const Container = styled.div`
   width: 100%;
-  margin-bottom: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 8rem;
+`;
+const PerfumeCntr = styled.div`
+  width: 80%;
+  flex-direction: column;
+  white-space: pre-wrap;
+  word-break: break-all;
+`;
+const Image = styled.img`
+  width: 8rem;
+  height: 9rem;
+  text-align: center;
+  align-tems: center;
+  background: url(${({ src }) => src}) center center / cover;
+`;
+
+const BascInfoCntr = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  justify-content: center;
+  margin-left: 0.2rem;
+  width: 9.5rem;
+  height: 10rem;
 `;
 const Text = styled.div`
-  width: 8rem;
-  text-align: left;
-  margin-left: 1.2rem;
-
-  font-family: Roboto;
-  font-size: 1rem;
-  font-weight: 700;
-  letter-spacing: -0.035rem;
+  font-size: ${(props) =>
+    props.isBig ? '1.1' : props.isMiddle ? '1' : '0.8'}rem;
+  margin-bottom: ${(props) =>
+    props.isBig ? '1.5' : props.isMiddle ? '1.2' : '0'}rem;
   color: ${(props) => (props.isGray ? 'gray' : 'black')};
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
+`;
+const InfoCntr = styled.div`
+  margin-top: 2rem;
 `;
 
-const CodyList = styled.div`
-  width: 100%;
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  justify-content: center;
-  column-gap: 0.5rem;
-`;
-
-const GoodsCntnr = styled.div`
-  margin-bottom: 1.5rem;
+const DetailInfo = styled.div`
+  font-weight: ${(props) => (props.isBold ? '700' : '')};
+  margin-bottom: ${(props) => (props.isBold ? '1' : '3')}rem;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-`;
-const IconCntr = styled.div`
-  z-index: 1;
-  position: relative;
-  margin-top: -2rem;
-  background-color: #eeeeee;
-  display: flex;
-  flex: 1;
-  width: 1.7rem;
-  height: 1.7rem;
-  margin-left: 10rem;
-  align-items: center;
-  justify-items: center;
   justify-content: center;
+  font-size: 0.9rem;
+  white-space: pre-wrap;
+  word-break: break-all;
+  background-color: ${(props) => (props.isGray ? '#dddddd' : '')};
+  width: ${(props) => (props.isGray ? `calc(100% -2rem)` : '100%')};
+  padding: ${(props) => (props.isGray ? '0.5rem 1rem' : '')};
 `;
-const HeartIcon = styled(AiFillHeart)`
-  font-size: 1.2rem;
-  color: ${(props) => (props.isPick ? 'red' : 'white')};
+
+const NoteInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 3rem;
+  justify-content: center;
+  font-size: 0.9rem;
+  white-space: pre-wrap;
+  word-break: break-all;
+  margin-bottom: 0.5rem;
+  border-bottom: ${(props) => !props.isLast && 'solid 0.1rem #dedede'};
+`;
+
+const DockBarCntr = styled.div`
+  position: fixed;
+  bottom: 0;
+  z-index: 5;
+  width: ${`calc(100% - 0.1rem);`};
+  height: 3rem;
+  background-color: white;
+  box-shadow: 0 0 0.3rem rgba(0, 0, 0, 0.1);
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  font-size: 1.8rem;
+  font-weight: 700;
+  letter-spacing: -0.045rem;
+  color: black;
+  border: 0.1rem solid #cdcdcd;
+`;
+
+const BtnCntnr = styled.div`
+  width: 50%;
+  height: 3rem;
+  border-right: 0.1rem solid #cdcdcd;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const BtnText = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  font-size: 0.9rem;
+`;
+const StarIcon = styled(AiFillStar)`
+  color: ${(props) => (props.isPick ? '#F6BE00' : 'white')};
   text-shadow: 0 0 1rem black;
   stroke: ${(props) => (props.isPick ? 'none' : 'black')};
   stroke-width: 2.5rem;
-`;
-const CartBtn = styled.img`
-  position: relative;
-  z-index: 2;
-  top: -4rem;
-  left: 11.2rem;
-
-  width: 3rem;
-  height: 3rem;
-  border-radius: 1.5rem;
-  stroke: black;
-  stroek-width: 1rem;
-`;
-
-const Btn = styled.button`
-  width: 5rem;
-  height: 2rem;
-  box-shadow: ${(props) =>
-    props.isShow ? '0 -0.5rem 1rem 0.2rem rgba(0, 0, 0, 0.3)' : 'none'};
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  font-size: 0.8rem;
-  color: black;
-  float: right;
-`;
-
-const Line = styled.div`
-  margin-top: 1rem;
-  border-bottom: ${(props) =>
-    props.isLast ? 'none' : props.isTwo ? 'none' : '1rem solid  #F4F4F4'};
-  width: 110%;
-  margin-left: -5%;
 `;
